3e41cdcf1fdb595203d839c5880f507b3170f47c
Rodrigo Jordao <rodrigo.jordao@gmail.com>
2016-07-16 21:09:50 -0400

    Quotes spring boot app initial commit

---
+++ src/main/java/io/github/jordao76/quotes/Application.java
@@ -0,0 +1,13 @@
+package io.github.jordao76.quotes;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+}
