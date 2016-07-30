657690fe5104125892d94a8ca5106330983e4f6c
Rodrigo Jordao <rodrigo.jordao@gmail.com>
2016-07-29 22:43:35 -0400

    Update spring-boot version to 1.4.0

--- src/test/java/io/github/jordao76/quotes/QuoteControllerTest.java
+++ src/test/java/io/github/jordao76/quotes/QuoteControllerTest.java
@@ -10,17 +10,15 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
 import org.junit.*;
 import org.junit.runner.*;
 import org.springframework.beans.factory.annotation.*;
-import org.springframework.boot.test.*;
+import org.springframework.boot.test.context.*;
 import org.springframework.test.context.junit4.*;
-import org.springframework.test.context.web.*;
 import org.springframework.test.web.servlet.*;
 import org.springframework.web.context.*;
 
 import io.github.jordao76.quotes.domain.*;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = QuoteApplication.class)
-@WebAppConfiguration
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class QuoteControllerTest {
 
   @Autowired
