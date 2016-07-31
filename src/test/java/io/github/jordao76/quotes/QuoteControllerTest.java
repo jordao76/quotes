25c793ca5936c053403d36fbe6cc9ca3dfe1958c
Rodrigo Jordao <rodrigo.jordao@gmail.com>
2016-07-30 23:52:42 -0400

    Update reference to code that creates initial quotes

--- src/test/java/io/github/jordao76/quotes/QuoteControllerTest.java
+++ src/test/java/io/github/jordao76/quotes/QuoteControllerTest.java
@@ -31,7 +31,7 @@ public class QuoteControllerTest {
     client = webAppContextSetup(wac).build();
   }
 
-  // first quote added to the repository (@see QuoteApplication.populateRepo),
+  // first quote added to the repository (@see QuoteRepositoryInitializer),
   // should have ID = 1
   String firstQuoteText = "Any sufficiently advanced technology is indistinguishable from magic.";
   String firstQuoteAuthor = "Arthur C. Clarke";
