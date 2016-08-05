934ce1cfad61a74a15bd0b1cb0c0a63e3274dec4
Rodrigo Jordao <rodrigo.jordao@gmail.com>
2016-08-04 22:50:19 -0400

    Add simple request logging
    - logging with DEBUG level the response status, request method and
      endpoint: all at response time
    - the logging is still inconsistent when a client error occurs,
      compare:
      - `curl localhost:8080/quotes/anyasdf` and
      - `curl -H "Content-Type: application/json" -X POST -d
          '{"text":"to be"}' localhost:8080/quotes`
    - it would be good to create tests to assert on the expected log output
    for each situation

---
+++ src/main/java/io/github/jordao76/quotes/filters/RequestLoggingFilter.java
@@ -0,0 +1,38 @@
+package io.github.jordao76.quotes.filters;
+
+import java.io.*;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+import org.slf4j.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.filter.*;
+
+@Component
+public class RequestLoggingFilter extends OncePerRequestFilter {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(RequestLoggingFilter.class);
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+      throws ServletException, IOException {
+
+    filterChain.doFilter(request, response);
+
+    if (LOGGER.isDebugEnabled()) {
+      LOGGER.debug("HTTP {} for {} {}",
+        value("status", response.getStatus()),
+        value("method", request.getMethod()),
+        value("endpoint", request.getRequestURI()));
+    }
+
+  }
+
+  // note: later to be replaced,
+  // see https://github.com/logstash/logstash-logback-encoder/tree/logstash-logback-encoder-4.7#loggingevent_custom_event
+  private Object value(String key, Object value) {
+    return value;
+  }
+
+}
